828cf4a6d61c4af5d9a99bf936ca5ee32b20f9d0
Woomer <119552156+Woomer@users.noreply.github.com>
2024-06-20 15:20:33 +0530

    Update settings.py

--- youtube_manager/settings.py
+++ youtube_manager/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-INSTALLED_APPS += ['rest_framework', 'corsheaders', 'core', 'uploader', 'Youtube_manager']
+INSTALLED_APPS += ['rest_framework', 'corsheaders', 'core', 'Youtube_manager']
 
 MIDDLEWARE += ['corsheaders.middleware.CorsMiddleware']
 
